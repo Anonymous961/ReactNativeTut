1447b5c6fdce2bf3e8961317fdf646d79207f2e7
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-05-09 20:30:45 +0530

    learnt about view and text components

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>
+        Hello World
+        </Text>
+      </View>
+      <View style={styles.body}>
+        <Text>This is just to check body content!</Text>
+        <Text>This is just to check body content!</Text>
+        <Text>This is just to check body content!</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +25,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor:"pink",
+    padding:30
+  },
+  boldText:{
+    fontWeight: "bold"
+  },
+  body:{
+    backgroundColor:"yellow",
+    padding:20
+  }
 });
