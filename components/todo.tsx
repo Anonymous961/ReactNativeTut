13724416c9ff7b3ef14e83a3ec4ee6021a164195
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-05-12 23:38:58 +0530

    adding delete icon

--- components/todo.tsx
+++ components/todo.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, ScrollView,FlatList, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
+
 const Todos = ({item,pressHandler}:any) => {
     return ( 
         <TouchableOpacity onPress={()=>pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+                <Text style={styles.itemText}>{item.text}</Text>
+                <MaterialIcons name="delete" size={24} color="black" />
+            </View>
         </TouchableOpacity>
      );
 }
@@ -18,5 +23,10 @@ const styles=StyleSheet.create({
         borderWidth:1,
         borderStyle:'dashed',
         borderRadius:10,
+        flexDirection:"row",
+        justifyContent:"space-between"
+    },
+    itemText:{
+        marginLeft:10
     }
 })
